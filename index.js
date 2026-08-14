44a1c14ccbcbb5e1561b00696c5bcdb09a2cf293
bo5135620-sys <bo5135620@gmail.com>
2026-08-14 17:25:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const { Client, GatewayIntentBits, EmbedBuilder, REST, Routes } = require('disco
 const http = require('http');
 http.createServer((req, res) => res.end('Bot Aktif')).listen(process.env.PORT || 10000);
 
-const FOTO_LINK = "https://cdn.discordapp.com/attachments/1536696056769814642/1537824750414463006/image.png?ex=6a807267&is=6a7f20e7&hm=62da9d8ce1ffb1656bc881786d51c4ae269b39f1f9a80bd9a259367d376336fa&"; // <--- FOTO LİNKİNİ BURAYA KOY
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('ready', async () => {
   console.log(`Giriş yapıldı: ${client.user.tag}`);
   const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [{ name: 'yaz', description: 'duyuru' }] });
+  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+    body: [
+      { name: 'yaz', description: 'emu duyurusu atar' },
+      { name: 'spoof', description: 'spoof duyurusu atar' }
+    ]
+  });
 });
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
+
+  // 1. KOMUT - /yaz
   if (interaction.commandName === 'yaz') {
     const embed = new EmbedBuilder()
 .setTitle('Slawes Cheats Emu-Private')
@@ -39,11 +44,34 @@ Desteklenen İşlemciler (CPU):
 Intel ve AMD
 Desteklenen Ekran Kartları (GPU):
 Intel ve AMD`)
-.setColor(0x8A2BE2)
-.setImage(FOTO_LINK)
-.setTimestamp();
-
+.setColor(0x8A2BE2);
     await interaction.reply({ embeds: [embed] });
   }
+
+  // 2. KOMUT - /spoof - FOTOLU OLAN
+  if (interaction.commandName === 'spoof') {
+    const embed2 = new EmbedBuilder()
+.setTitle('Slawes Cheats Spoofer')
+.setDescription(`Şunlarla çalışır:
+SlawesCheats Spoofer
+TPM ,SECURE BOOT ,HVCI ,IOMMU(VT-D) : On
+TÜM ANAKARTLARLA UYUMLU!
+DRIVERLESS!
+TEK SEFERLİK KALICI BİR ÇÖZÜM!
+VAN 152, VAL5 KESİN ÇÖZÜM!
+ÇIK GİR VEYA POPUP HATASI YOK!
+KALICI TPM BYPASS
+KOLAY KURULUM VE KLAVUZ
+KALICI TPM BYPASS﻿
+HWID BAN KALDIRMA
+DONANIM BANI ATAN TÜM OYUNLARDAN TEK TUŞ İLE KURTULMA İMKANI !!
+
+Not:Bu Ürünün Doğru Çalışması İçin Windows Reİnstall Ve Bios Flash gereklidir VE RAİD ATMANIZ ŞART !
+eğer bunlar ne bilmiyorsanız satın alım sonrası destek ekibimiz size tüm adımlarda yardımcı olacaktır Asus ve Laptoplarda Raid şart değildir msi asrock gigbyte raid şartır`)
+.setColor(0xFF0000)
+.setImage('https://cdn.discordapp.com/attachments/1536696056769814642/1537828399140053152/08cf01ea-1927-443a-8ce0-2e84770d57ba.jpg?ex=6a8075cd&is=6a7f244d&hm=103a57edf171a93feff03ec7e539b657be281909536f7310c9f063d491bf071d&');
+    await interaction.reply({ embeds:  });
+  }
+
 });
 client.login(process.env.TOKEN);
