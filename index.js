27b987fc631637f2f41b81d458e3951341a29b9a
bo5135620-sys <bo5135620@gmail.com>
2026-08-14 17:11:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
 const { Client, GatewayIntentBits, EmbedBuilder, REST, Routes } = require('discord.js');
 const http = require('http');
 http.createServer((req, res) => res.end('Bot Aktif')).listen(process.env.PORT || 10000);
+
+const FOTO_LINK = "https://cdn.discordapp.com/attachments/1536696056769814642/1537824750414463006/image.png?ex=6a807267&is=6a7f20e7&hm=62da9d8ce1ffb1656bc881786d51c4ae269b39f1f9a80bd9a259367d376336fa&"; // <--- FOTO LİNKİNİ BURAYA KOY
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('ready', async () => {
   console.log(`Giriş yapıldı: ${client.user.tag}`);
   const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [{ name: 'yaz', description: 'duyuru atar' }] });
+  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [{ name: 'yaz', description: 'duyuru' }] });
 });
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
   if (interaction.commandName === 'yaz') {
     const embed = new EmbedBuilder()
-   .setAuthor({ name: 'Slawes Cheats | UYG' })
-   .setTitle('Slawes Cheats Emulator v1.0')
-   .setDescription(
-`Merhabalar, sizlere Slawes Klanın geliştirdiği Emulator sunuyorum. Emulator val 5 vermez veya van 156 verirse ticket açın olmaz eğer olursa ticket açıp fixlenmesini bekleyin kesinlikle kalıcıdır iyi günler.
+.setTitle('Slawes Cheats Emu-Private')
+.setDescription(`Şunlarla çalışır:
+Valorant
+League of Legends
+Hanbot
+Scriptler
 
-🌟 **Özellikler**
-> İsterseniz vanguardı silin
-> Eğer Val 5 Verirse Tekrar Deneyin
-> Gene Olursa Ticket açınca fixlenmeyi bekleyin
-> van 102 ard arda verirse biraz bekleyin sonra tekrar deneyin`
-    )
-   .addFields(
-      { name: '🎮 Desteklenen Oyunlar', value: '• Valorant (Vanguard)', inline: true },
-      { name: '🔧 Neler Durdur?', value: '• vgc (durdurur)\n• vgk (durdurur)\n• vanguard (durdurur)', inline: true }
-    )
-   .setColor(0x8A2BE2)
-   .setTimestamp();
+Özellikler:
+Temiz ortam emülasyonu
+Windows yeniden başlatma gerektirmez
+BIOS flashlama gerektirmez
+Secure Boot kapatma gerektirmez
+TPM kapatma gerektirmez
+HVCI kapatma gerektirmez
+VGC kaldırma gerektirmez
+Emülatör kullanımı için tek bilgisayar sınırı
+VGK Heartbeat (PACMEN) Emülasyonu
+
+Windows 10 ve 11
+Desteklenen İşlemciler (CPU):
+Intel ve AMD
+Desteklenen Ekran Kartları (GPU):
+Intel ve AMD`)
+.setColor(0x8A2BE2)
+.setImage(FOTO_LINK)
+.setTimestamp();
+
     await interaction.reply({ embeds: [embed] });
   }
 });
